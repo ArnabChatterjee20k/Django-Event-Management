c867475f4bec953dfc4b5aef702f51d5a979b1e9
arnab <arnabchatterjee.ac.2@gmail.com>
2023-11-19 13:01:01 +0530

    feat -- add serialisation to reduce the size of the payload being sent

--- backend/event_management/event_search/serialiser.py
+++ backend/event_management/event_search/serialiser.py
@@ -1,6 +1,26 @@
-from rest_framework.serializers import Serializer ,CharField , IntegerField , ChoiceField
+from rest_framework.serializers import Serializer,BaseSerializer ,CharField , IntegerField , ChoiceField ,DateTimeField
+from pprint import pprint
 class EventSearchSerialiser(Serializer):
     keyword = CharField()
     distance = IntegerField()
     category = ChoiceField(choices=["default","sports","music"])
     location = CharField()
+
+class EventSearchOutputSerialiser(Serializer):
+    id = CharField()
+    event = CharField()
+    date_time = CharField()
+    genre = CharField()
+    venue = CharField()
+    icon = CharField()
+    
+    def to_internal_value(self, data):
+        obj = dict()
+        obj["id"] = data.get("id")
+        obj["event"] = data.get("name")
+        obj["date_time"] = data.get("dates").get("start").get("dateTime")
+        obj["genre"] = data.get("classifications")[0].get("genre").get("name")
+        obj["venue"] = data.get("_embedded").get("venues")[0].get("name")
+        obj["icon"] = data.get("images")[0].get("url")
+        return super().to_internal_value(obj)
+        
--- backend/event_management/event_search/utils/Events.py
+++ backend/event_management/event_search/utils/Events.py
@@ -1,6 +1,8 @@
 import os
 import requests
 from .Maps import Maps
+from ..serialiser import EventSearchOutputSerialiser
+
 class Events:
     def __init__(self):
         self.API_KEY = os.environ.get("TICKETMASTER_API_KEY")
@@ -15,10 +17,18 @@ class Events:
             "unit":"miles",
             "geoPoint":self.map_client.get_geocode(location)
         }
-        print(self.map_client.get_geocode(location))
+        all_events = self._get_all_events(eventSearchParams).get("_embedded").get("events")
+        
+        
+        all_events_data_with_required_fields = EventSearchOutputSerialiser(data=all_events,many=True)
+        all_events_data_with_required_fields.is_valid(raise_exception=True)
+        
+        return all_events_data_with_required_fields.validated_data
+        
+
+    def _get_all_events(self,eventSearchParams):
         endpoint = "events"
         url = self._build_FULL_URL(endpoint,**eventSearchParams)
-        print(url)
         response = requests.get(url)
         if response.status_code != 200:
             raise Exception("Error while fetching the events")
@@ -26,8 +36,7 @@ class Events:
 
     def _build_FULL_URL(self,endpoint,**kwargs):
         """The structure of TicketMaster HOST/ENDPOINT?apikey={}"""
-        base = f"{self.API_URL}/{endpoint}?apikey={self.API_KEY}&size=10"
+        base = f"{self.API_URL}/{endpoint}?apikey={self.API_KEY}&size=20"
         for key in kwargs:
             base+=f"&{key}={kwargs[key]}"
-        print(base)
         return base
