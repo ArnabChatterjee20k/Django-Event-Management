e14901e3bd8de28548800b86866e26758454fdaf
arnab <arnabchatterjee.ac.2@gmail.com>
2023-11-20 11:30:27 +0530

    feat -- updated serialser for the event details

--- backend/event_management/event_search/serialiser.py
+++ backend/event_management/event_search/serialiser.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import Serializer,BaseSerializer ,CharField , IntegerField , ChoiceField ,DictField
-from pprint import pprint
+from .utils.getter import getter
+from .utils.joinStringBySep import joinStringBySep
 class EventSearchSerialiser(Serializer):
     keyword = CharField()
     distance = IntegerField()
@@ -30,34 +31,58 @@ class EventSearchOutputSerialiser(Serializer):
         return super().to_internal_value(obj)
 
 class EventDetailsSerialiser(Serializer):
-    name : CharField()
-    date_time = DictField()
-    artists_team = CharField(default="")
-    venue = CharField(default="")
-    genre = CharField(default="")
+    name : CharField(allow_blank=True,default="")
+    date_time = DictField(default={})
+    artists_team = CharField(allow_blank=True,default="")
+    venue = DictField(default={})
+    genre = CharField(allow_blank=True,default="")
     price_ranges = DictField(default={})
-    ticket_link = CharField(default="")
-    seat_map = CharField(default="")
-    ticket_status = CharField(default="")
+    ticket_link = CharField(allow_blank=True,default="")
+    seat_map = CharField(allow_blank=True,default="")
+    ticket_status = CharField(allow_blank=True,default="")
+    open_hours = CharField(allow_blank=True,default="")
+    phone_number = CharField(allow_blank=True,default="")
+    general_rule = CharField(allow_blank=True,default="")
+    child_rule = CharField(allow_blank=True,default="")
+
 
     def to_internal_value(self, data):
         obj = dict()
-        obj["name"] = data.get("name")
+        obj["name"] = getter(data, "name")
+        obj["ticket_link"] = getter(data, "url")
 
-        dates = data.get("dates").get("start")
+        dates = getter(data, "dates.start")
         obj["date_time"] = {
-            "localDate":dates.get("localDate"),
-            "localTime":dates.get("localTime"),
-
+            "localDate": getter(dates, "localDate"),
+            "localTime": getter(dates, "localTime"),
         }
 
-        obj["venue"] = data.get("_embedded").get("venues")[0].get("name")
-        obj["genre"] = data.get("classifications")[0].get("genre").get("name")
-        
-        price_ranges = data.get("priceRanges")[0]
-        obj["price_ranges"] = {"max":price_ranges.get("max"),"min":price_ranges.get("min")}
-        
-        obj["seat_map"] = data.get("seatmap").get("staticUrl")
+        venue = getter(data, "_embedded.venues.0")
+        obj["venue"] = {
+            "name": getter(venue, "name"),
+            "city": getter(venue, "city.name"),
+            "state": getter(venue, "state.name"),
+            "address": getter(venue, "address.line1"),
+            "location": {"lat": getter(venue, "location.latitude"), "long": getter(venue, "location.longitude")}
+        }
+        segment = getter(data, "classifications.0.segment.name")    
+        genre = getter(data, "classifications.0.genre.name")    
+        subGenre = getter(data, "classifications.0.subGenre.name")
+        type = getter(data, "classifications.0.type.name")
+        subType = getter(data, "classifications.0.subType.name")
 
-        obj["ticket_status"] = data.get("dates").get("status").get("code")
+        obj["genre"] = joinStringBySep(strs=(segment,genre,subGenre,type,subType))
+
+        obj["price_ranges"] = {"max": getter(data, "priceRanges.0.max"), "min": getter(data, "priceRanges.0.min")}
+
+        obj["seat_map"] = getter(data, "seatmap.staticUrl")
+
+        obj["ticket_status"] = getter(data, "dates.status.code")
+
+        obj["open_hours"] = getter(data,"_embedded.boxOfficeInfo.openHoursDetail")
+        
+        obj["phone_number"] = getter(data,"_embedded.venues.0.boxOfficeInfo.phoneNumberDetail")
+        
+        obj["general_rule"] = getter(data,"_embedded.venues.0.generalInfo.generalRule")
+        obj["child_rule"] = getter(data,"_embedded.venues.0.generalInfo.childRule")
         return super().to_internal_value(obj)
